deafb287cd2879a9ccc281cc4d00e70e74819ee8
theZWbutDisconnected <2090252203@qq.com>
2024-09-19 01:42:50 +0800

    Update ChartingState.hx

--- source/meta/state/charting/ChartingState.hx
+++ source/meta/state/charting/ChartingState.hx
@@ -532,7 +532,11 @@ class ChartingState extends MusicBeatState
 
 	private function generateBackground()
 	{
-		coolGrid = new FlxBackdrop(null, 1, 1, true, true, 1, 1);
+		#if (flixel_addons <= "2.12.1")
+		coolGrid = new FlxBackdrop(null, 1, 1, true, false);
+		#else
+		coolGrid = new FlxBackdrop(null, XY, 1, 1);		
+		#end
 		coolGrid.loadGraphic(Paths.image('UI/forever/base/chart editor/grid'));
 		coolGrid.alpha = (32 / 255);
 		add(coolGrid);
