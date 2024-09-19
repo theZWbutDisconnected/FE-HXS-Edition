c0f218061cf00c87a28b3eb6c37ad9f8d0aa1189
theZWbutDisconnected <2090252203@qq.com>
2024-09-19 13:16:20 +0800

    Add files via upload

---
+++ source/import.hx
@@ -0,0 +1,10 @@
+#if android
+import android.content.Context;
+import android.widget.Toast;
+import android.os.Environment;
+import android.Permissions;
+import lime.app.Application;
+import backend.AppData;
+#end
+
+using StringTools;
