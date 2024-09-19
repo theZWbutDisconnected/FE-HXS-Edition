7cde59d92ccfd6ae75cc5ff5c7f58dc9ae3dc41e
theZWbutDisconnected <2090252203@qq.com>
2024-09-19 20:56:48 +0800

    Add files via upload

--- source/android/backend/StorageUtil.hx
+++ source/android/backend/StorageUtil.hx
@@ -4,6 +4,29 @@ import lime.system.System as LimeSystem;
 import haxe.io.Path;
 import haxe.Exception;
 
+#if android
+import android.content.Context as AndroidContext;
+import android.widget.Toast as AndroidToast;
+import android.os.Environment as AndroidEnvironment;
+import android.Permissions as AndroidPermissions;
+import android.Settings as AndroidSettings;
+import android.Tools as AndroidTools;
+import android.os.Build.VERSION as AndroidVersion;
+import android.os.Build.VERSION_CODES as AndroidVersionCode;
+import android.os.BatteryManager as AndroidBatteryManager;
+
+import android.backend.StorageUtil;
+#end
+
+#if sys
+import sys.*;
+import sys.io.*;
+#end
+
+import meta.CoolUtil;
+
+using StringTools;
+
 /**
  * A storage class for mobile.
  * @author Mihai Alexandru (M.A. Jigsaw), Karim Akra and Lily Ross (mcagabe19)
