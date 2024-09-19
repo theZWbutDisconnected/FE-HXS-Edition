82d10b9f8f87e76e8f5236c11e65a49dc24fde24
theZWbutDisconnected <2090252203@qq.com>
2024-09-19 21:41:09 +0800

    Update Init.hx

--- source/Init.hx
+++ source/Init.hx
@@ -12,29 +12,6 @@ import meta.state.charting.*;
 import openfl.filters.BitmapFilter;
 import openfl.filters.ColorMatrixFilter;
 
-#if android
-import android.content.Context as AndroidContext;
-import android.widget.Toast as AndroidToast;
-import android.os.Environment as AndroidEnvironment;
-import android.Permissions as AndroidPermissions;
-import android.Settings as AndroidSettings;
-import android.Tools as AndroidTools;
-import android.os.Build.VERSION as AndroidVersion;
-import android.os.Build.VERSION_CODES as AndroidVersionCode;
-import android.os.BatteryManager as AndroidBatteryManager;
-
-import android.backend.StorageUtil;
-#end
-
-#if sys
-import sys.*;
-import sys.io.*;
-#end
-
-import meta.CoolUtil;
-
-using StringTools;
-
 /** 
 	Enumerator for settingtypes
 **/
