7cb951d9ed8aee4e4876015eb229f039c76c9aeb
theZWbutDisconnected <2090252203@qq.com>
2024-09-22 15:03:11 +0800

    path更新 #5

--- source/meta/CoolUtil.hx
+++ source/meta/CoolUtil.hx
@@ -115,7 +115,7 @@ class CoolUtil
 		forEachDirectory(mainDirectory, type);
 	    var assetPaths = externalAssetsTemp;
 		externalAssetsTemp = [];
-		CoolUtil.showPopUp(Std.string(assetsPath), 'Getting External Assets');
+		CoolUtil.showPopUp(Std.string(assetPaths), 'Getting External Assets');
 		return assetPaths;
 	}
 	
