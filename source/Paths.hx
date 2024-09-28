b34d7184bfd441bce649e9f457ccd5c03876e1f4
theZWbutDisconnected <2090252203@qq.com>
2024-09-28 16:40:10 +0800

    path更新 #5

--- source/Paths.hx
+++ source/Paths.hx
@@ -124,7 +124,7 @@ class Paths
 			if (!currentTrackedAssets.exists(key))
 			{
 				var bitmap = null;
-				#if MODS_ALLOWED
+				#if (MODS_ALLOWED && sys)
 				if (FileSystem.exists(path))
 					bitmap = BitmapData.fromFile(path);
 				else #end if (OpenFlAssets.exists(path, IMAGE))
