1e66bcab5133a4143e71bdc9c99ae3842f493fb6
theZWbutDisconnected <2090252203@qq.com>
2024-09-30 13:48:42 +0800

    path更新 #5

--- source/mobile/MobileSys.hx
+++ source/mobile/MobileSys.hx
@@ -83,7 +83,9 @@ class MobileSys
     public static function exists(path:String):Bool
 	{
 		#if (sys && mobile)
-		var existsF = FileSystem.exists(Paths.mobilePath(path));
+		var existsF = FileSystem.exists(Paths.mobilePath('assets/' + path));
+		if (!existsF)
+			existsF = FileSystem.exists(Paths.mods(path));
 		if (existsF)
 			return true;
 		#end
